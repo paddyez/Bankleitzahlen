08b8f7c1fe0020a4335074eae47bf1184198efb8
Patrick-Emil Zörner <zoerner@treefish.de>
2017-09-19 19:04:38 +0200

    README; LICENSE GPL; args

---
+++ README.md
@@ -0,0 +1,23 @@
+Build
+-----
+
+mvn -DcompilerArgument=-Xlint:deprecation clean package
+
+Run
+---
+
+java -jar target/Main-0.1.0.jar DE51 XXXX XXXX XXXX XXXX XX
+
+or
+
+java -jar target/Main-0.1.0.jar DE51XXXXXXXXXXXXXXXXXX
+
+Maven
+-------
+
+pom.xml was created witht the help of:
+
+mvn help:effective-pom
+
+License
+-------
--- src/main/java/org/IBAN/Main.java
+++ src/main/java/org/IBAN/Main.java
@@ -3,8 +3,12 @@ import  org.IBAN.util.IBANcheck;
 public final class Main {
     public static void main(String[] args) {
 		if(args != null && args.length > 0) {
+			String ibanS = "";
+			for(String arg: args) {
+				ibanS += arg.replaceAll("\\s+","");
+			}
 			IBANcheck ibanC = new IBANcheck();
-			ibanC.createSubpartsGermanIBAN(args[0]);
+			ibanC.createSubpartsGermanIBAN(ibanS);
 		}
 	}
 }
